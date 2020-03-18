9bd033d585c592e8ecf3d4e719f711fa94994830
ccx1010sky <ccx1010@gmail.com>
2020-03-18 13:09:39 +0000

    first react/sass

---
+++ src/component/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Header() {
+  return (
+    // 如下<div className="Header"></div> 相当于<header>
+    <div className="Header">
+      <div class="overlay"></div>
+      <div class="menu-btn">
+        {/* <!-- div里面无内容，相当于一个白色区块 --> */}
+
+        <div class="btn-line"></div>
+        <div class="btn-line"></div>
+        <div class="btn-line"></div>
+      </div>
+      <div class="menu">
+        <div class="menu-branding">
+          <div class="img-portrait">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <ul>
+        <li class="nav-item">
+          <a href="/" class="nav-link">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/about.html" class="nav-link">
+            About Me
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/work.html" class="nav-link">
+            My Work
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/contact.html" class="nav-link">
+            How To Reach Me
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default Header;
---
+++ src/component/Main.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="Main">
+      <h1 class="lg-heading">
+        Nick
+        <span class="text-secondary">Chen</span>
+      </h1>
+      <h2 class="sm-heading">
+        Web Developer,Programmer,Designer,&enterpreneur
+      </h2>
+      <div class="icons">
+        <a href="#!">
+          <i class="fab fa-twitter fa-2x"></i>
+        </a>
+        <a href="#">
+          <i class="fab fa-facebook fa-2x"></i>
+        </a>
+        <a href="#!">
+          <i class="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="#!">
+          <i class="fab fa-github fa-2x"></i>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
