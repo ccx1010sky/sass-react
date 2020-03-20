e49dadaaa35ff1bbadfebd51491f2702cdd1268b
ccx1010sky <ccx1010@gmail.com>
2020-03-20 20:05:59 +0000

    Updates

---
+++ precache-manifest.4f035c355dc221dda36465c16c08a253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a771ffb681bfdd9d05d7c6e3704c21ae",
+    "url": "/react-sass/index.html"
+  },
+  {
+    "revision": "eb2302bdc829a925c24a",
+    "url": "/react-sass/static/css/main.a3d752b4.chunk.css"
+  },
+  {
+    "revision": "7eb68c7ce1a190780d0a",
+    "url": "/react-sass/static/js/2.ba0a1983.chunk.js"
+  },
+  {
+    "revision": "830c55e5e5effdba80df2f1b4bb9b6b9",
+    "url": "/react-sass/static/js/2.ba0a1983.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb2302bdc829a925c24a",
+    "url": "/react-sass/static/js/main.07c4cd37.chunk.js"
+  },
+  {
+    "revision": "87d8f4403e24e0edcdc2",
+    "url": "/react-sass/static/js/runtime-main.dcf148a6.js"
+  },
+  {
+    "revision": "a926a5f28b8a5e532e6c77df0f9a0b19",
+    "url": "/react-sass/static/media/background1.a926a5f2.jpeg"
+  }
+]);
